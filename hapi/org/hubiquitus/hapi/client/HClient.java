9f1e123f562d314a9f79efda00dd204aeaf5104d
Nadim <nadim.boustani@gmail.com>
2012-06-15 14:34:20 +0200

    amend commit be2346f474066225be99b464df9e88b50ab06219 and modified hclient builders

--- hapi/org/hubiquitus/hapi/client/HClient.java
+++ hapi/org/hubiquitus/hapi/client/HClient.java
@@ -311,15 +311,15 @@ public class HClient {
 	 * Helper to create hmessage
 	 * @see HMessage
 	 * @param chid - channel id : mandatory
-	 * @param type : mandatory
-	 * @param payload : mandatory
+	 * @param type
+	 * @param payload
 	 * @param options
 	 * @return hMessage
 	 */
 	public HMessage buildMessage(String chid, String type, HJsonObj payload, HMessageOptions options) {
 		HMessage hmessage = new HMessage();
 		if(this.connectionStatus == ConnectionStatus.CONNECTED) {
-			if(chid != null && type != null && payload != null) {
+			if(chid != null && chid.length() > 0) {
 				hmessage.setChid(chid);
 				hmessage.setConvid(options.getConvid());
 				hmessage.setType(type);
@@ -344,7 +344,7 @@ public class HClient {
 						hresult.setCmd("publish");
 						hresult.setStatus(ResultStatus.MISSING_ATTR);
 						HJsonDictionnary result = new HJsonDictionnary();
-						result.put("error", "missing attribut");
+						result.put("error", "missing attribut : chid");
 						hresult.setResult(result);
 						callback.hDelegate("hresult", hresult);
 					}
@@ -366,15 +366,15 @@ public class HClient {
 	/**
 	 * Helper to create hconv
 	 * @param chid - channel id : mandatory
-	 * @param topic : mandatory
-	 * @param participants : mandatory
+	 * @param topic
+	 * @param participants
 	 * @param options
 	 * @return hmessage
 	 */
 	public HMessage buildConv(String chid, String topic, List<String> participants, HMessageOptions options) {
 		HMessage hmessage = new HMessage();
 		if(this.connectionStatus == ConnectionStatus.CONNECTED) {
-			if(chid != null && topic != null && participants != null) {
+			if(chid != null  && chid.length()>0) {
 				HConv hconv = new HConv();
 				hconv.setTopic(topic);
 				hconv.setParticipants(participants);
@@ -386,7 +386,7 @@ public class HClient {
 						hresult.setCmd("publish");
 						hresult.setStatus(ResultStatus.MISSING_ATTR);
 						HJsonDictionnary result = new HJsonDictionnary();
-						result.put("error", "missing attribut");
+						result.put("error", "missing attribut : chid");
 						hresult.setResult(result);
 						callback.hDelegate("hresult", hresult);
 					}
@@ -416,7 +416,7 @@ public class HClient {
 	public HMessage buildAck(String chid, String ackid,HAckValue ack, HMessageOptions options) {
 		HMessage hmessage = new HMessage();
 		if(this.connectionStatus == ConnectionStatus.CONNECTED) {
-			if(chid != null && ackid != null && ack != null) {
+			if(chid != null && chid.length()>0 && ackid != null && ackid.length()>0 && ack != null) {
 				HAck hack = new HAck();
 				hack.setAckid(ackid);
 				hack.setAck(ack);
@@ -428,7 +428,7 @@ public class HClient {
 						hresult.setCmd("publish");
 						hresult.setStatus(ResultStatus.MISSING_ATTR);
 						HJsonDictionnary result = new HJsonDictionnary();
-						result.put("error", "missing attribut");
+						result.put("error", "missing attribut : chid, ackid or ack");
 						hresult.setResult(result);
 						callback.hDelegate("hresult", hresult);
 					}
@@ -457,7 +457,7 @@ public class HClient {
 	public HMessage buildAlert(String chid, String alert, HMessageOptions options) {
 		HMessage hmessage = new HMessage();
 		if(this.connectionStatus == ConnectionStatus.CONNECTED) {
-			if(chid != null && alert != null) {
+			if(chid != null && chid.length()>0 && alert != null  && alert.length()>0) {
 			HAlert halert = new HAlert();
 			halert.setAlert(alert);
 			hmessage = buildMessage(chid, "halert", halert, options);
@@ -468,7 +468,7 @@ public class HClient {
 						hresult.setCmd("publish");
 						hresult.setStatus(ResultStatus.MISSING_ATTR);
 						HJsonDictionnary result = new HJsonDictionnary();
-						result.put("error", "missing attribut");
+						result.put("error", "missing attribut : chid or alert");
 						hresult.setResult(result);
 						callback.hDelegate("hresult", hresult);
 					}
@@ -498,7 +498,8 @@ public class HClient {
 	public HMessage buildMeasure(String chid, String value, String unit, HMessageOptions options) {
 		HMessage hmessage = new HMessage();
 		if(this.connectionStatus == ConnectionStatus.CONNECTED) {
-			if(chid != null && value != null && unit != null) {
+			if(chid != null && chid.length() > 0 && value != null 
+				&& value.length() > 0 && unit != null && unit.length() > 0) {
 				HMeasure hmeasure = new HMeasure();
 				hmeasure.setValue(value);
 				hmeasure.setUnit(unit);
@@ -510,7 +511,7 @@ public class HClient {
 						hresult.setCmd("publish");
 						hresult.setStatus(ResultStatus.MISSING_ATTR);
 						HJsonDictionnary result = new HJsonDictionnary();
-						result.put("error", "missing attribut");
+						result.put("error", "missing attribut : chid, value or unit");
 						hresult.setResult(result);
 						callback.hDelegate("hresult", hresult);
 					}
