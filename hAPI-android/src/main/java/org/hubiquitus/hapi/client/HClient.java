1b52609a71ab28aca07d1f9413677131a4ab5b11
Chenliang <louis.scl@gmail.com>
2012-11-21 18:43:57 +0100

    delete setters of fulljid and resource

--- hAPI-android/src/main/java/org/hubiquitus/hapi/client/HClient.java
+++ hAPI-android/src/main/java/org/hubiquitus/hapi/client/HClient.java
@@ -98,21 +98,12 @@ public class HClient {
 	}
 
 
-	public void setFullJid(String fullJid) {
-		this.fullJid = fullJid;
-	}
-
 
 	public String getResource() {
 		return resource;
 	}
 
 
-	public void setResource(String resource) {
-		this.resource = resource;
-	}
-
-	
     public HClient() {
 		transportOptions = new HTransportOptions();
 		
@@ -155,8 +146,8 @@ public class HClient {
 			// fill HTransportOptions
 			try {
 				this.fillHTransportOptions(publisher, password, options);
-				setFullJid(this.transportOptions.getJid().getFullJID());
-				setResource(this.transportOptions.getResource());
+				this.fullJid = this.transportOptions.getJid().getFullJID();
+				this.resource = this.transportOptions.getResource();
 			} catch (Exception e) {
 				// stop connecting if filling error
 				this.notifyStatus(ConnectionStatus.DISCONNECTED, ConnectionError.JID_MALFORMAT, e.getMessage());
